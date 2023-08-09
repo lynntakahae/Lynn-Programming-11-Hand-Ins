376126073cf9fa66e1626e28caedda113b52af70
lynntakahae <takahaelynn@gmail.com>
2023-08-08 18:17:26 -0700

    Drawing With Text Assignment

---
+++ Drawing with text/src/Main.java
@@ -0,0 +1,10 @@
+//Drawing with Text - A cat
+//08/08/23
+//Lynn Takahae
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("  /\\_/\\");
+            System.out.println(" ( o.o )");
+            System.out.println("  > ^ <");
+        }
+    }
